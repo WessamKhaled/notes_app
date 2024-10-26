3600a41574b8198b4f1a3b79bae9bed3ad56659c
Wessam Khaled <94244278+WessamKhaled@users.noreply.github.com>
2024-10-26 17:42:30 +0300

    complite edit note view

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -17,8 +18,14 @@ class EditNoteViewBody extends StatelessWidget {
             icon: Icons.check,
             title: 'Edit Note',
           ),
+          CustomTextField(hint: 'Title'),
+          SizedBox(
+            height: 18,
+          ),
+          CustomTextField(hint: 'Content',MaxLine: 5,)
         ],
       ),
     );
   }
 }
+
